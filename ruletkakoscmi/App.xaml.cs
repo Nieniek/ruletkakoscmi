d24f442a866504bd361653287ec403e7c491eb62
Marek Błaszczyk <mareczekblaszczyk@gamil.com>
2024-09-23 12:42:12 +0200

    Dodaj pliki projektów.

---
+++ ruletkakoscmi/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ruletkakoscmi
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ruletkakoscmi/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿namespace ruletkakoscmi
+{
+    public partial class MainPage : ContentPage
+    {
+        int gameResult = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void diceroll(object sender, EventArgs e)
+        {
+            int[] dice = new int[5];
+
+            Random r = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                dice[i] = r.Next(1, 7);
+            }
+
+            Dice0.Source = "k6_" + dice[0] + ".png";
+            Dice1.Source = "k6_" + dice[1] + ".png";
+            Dice2.Source = "k6_" + dice[2] + ".png";
+            Dice3.Source = "k6_" + dice[3] + ".png";
+            Dice4.Source = "k6_" + dice[4] + ".png";
+
+            int rollResult = 0;
+
+            for (int dots = 1; dots < 6; dots++)
+            {
+                int count = 0;
+                for (int diceIndex = 0; diceIndex < 5; diceIndex++)
+                {
+                    if (dice[diceIndex] == dots)
+                    {
+                        count++;
+                    }
+
+                    if (count > 1)
+                    {
+                        rollResult += dots * count;
+                    }
+                }
+                RollResultLabel.Text = "Wynik tego losowania: " + rollResult;
+                gameResult += rollResult;
+                GameResultLabel.Text = "Wynik gry: " + gameResult;
+            }
+        }
+
+      
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Dice0.Source = "k6_0.png";
+            Dice1.Source = "k6_0.png";
+            Dice2.Source = "k6_0.png";
+            Dice3.Source = "k6_0.png";
+            Dice4.Source = "k6_0.png";
+            gameResult = 0;
+            RollResultLabel.Text = "Wynik tego losowania: 0";
+            GameResultLabel.Text = "Wynik gry: " + gameResult;
+        }
+    }
+}
+    
+
+
